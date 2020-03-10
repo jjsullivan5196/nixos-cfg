eef167383642366af5d3e65fe1740aa4966e4255
John Sullivan <jsullivan@csumb.edu>
2020-03-10 16:53:47 -0700

    Moving past my nanny

--- block.nix
+++ block.nix
@@ -1,14 +1,4 @@
 { config, lib, pkgs, ... }:
 
 {
-  # Get rid of these sites
-  networking.hosts = {
-    "0.0.0.0" = [
-      # Reddit
-      "reddit.com"
-      "www.reddit.com"
-      "old.reddit.com"
-      "i.reddit.com"
-    ];
-  };
 }
